c40cdfc32e4c0fc27f2be82317f8baea50333698
pbsopankar <pavansopankar123@gmail.com>
2026-05-01 01:12:35 -0400

    Create ResponseAnalyzer class to analyze API responses

---
+++ src/response_analyzer.py
@@ -0,0 +1,21 @@
+class ResponseAnalyzer:
+    def __init__(self, response):
+        self.response = response
+
+    def analyze_security_indicators(self):
+        # Logic to analyze the response for security indicators
+        pass
+
+    def detect_error_patterns(self):
+        # Logic to detect error patterns in the response
+        pass
+
+    def identify_vulnerability_signatures(self):
+        # Logic to identify vulnerability signatures
+        pass
+
+    def analyze(self):
+        self.analyze_security_indicators()
+        self.detect_error_patterns()
+        self.identify_vulnerability_signatures()
+        # Additional analysis logic
